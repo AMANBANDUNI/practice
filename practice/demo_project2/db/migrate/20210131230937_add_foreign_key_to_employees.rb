2e69107f6548a9afc03557d4c50d2679062bc535
amanbanduni <abanduni.5@gmail.com>
2021-02-01 07:25:38 +0530

    Added a foreign key Qualification_id to employees table

--- practice/demo_project2/db/migrate/20210131230937_add_foreign_key_to_employees.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToEmployees < ActiveRecord::Migration[6.1]
-  def change
-    add_column :employees, :qualification_id, :integer, index: true
-    add_foreign_key :employees, :qualifications, column: :qualification_id
-  end
-end
---
+++ practice/demo_project2/db/migrate/20210201012926_add_foreign_key_qualification_id_to_employees.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyQualificationIdToEmployees < ActiveRecord::Migration[6.1]
+  def change
+  	add_reference :employees, :qualification, foreign_key: true
+  end
+end
